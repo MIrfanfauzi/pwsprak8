14f29ab6f653ceb26e956dea85b2dbbe342902b5
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-11-21 15:11:04 +0700

    menambahkan koneksi ke database

---
+++ config/db.js
@@ -0,0 +1,16 @@
+// config/db.js
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || '127.0.0.1',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '',        
+  database: process.env.DB_DATABASE || 'apikey_db', 
+  port: parseInt(process.env.DB_PORT, 10) || 3309, 
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool;
